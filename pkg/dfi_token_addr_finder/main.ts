3e4a46c0b8dc70b8f9adfadfb5a8a147c53adce8
Prasanna Loganathar <pvl@prasannavl.com>
2024-07-25 15:15:25 +0800

    Add justfiles for deno

--- pkg/dfi_token_addr_finder/main.ts
+++ pkg/dfi_token_addr_finder/main.ts
@@ -1,19 +1,61 @@
 import { parseArgs } from "@std/cli/parse-args";
-import { CliDriver, GetPoolPairResponse, initTimedLogger, ListTokensItem, TokenAmount } from "@pkg/dfi_sdk_ts/lib.ts";
+import {
+  CliDriver,
+  GetPoolPairResponse,
+  initTimedLogger,
+  ListTokensItem,
+  TokenAmount,
+} from "@pkg/dfi_sdk_ts/lib.ts";
+
+function loadOpts() {
+  const alias = {
+    "help": "h",
+    "filter": "f",
+  };
+  const args = parseArgs(Deno.args, {
+    alias,
+    string: ["filter"],
+    boolean: ["help"],
+    "--": false,
+  });
+  const getHelpText = () => {
+    let text =
+      "Outputs all the addresses who are holders of the token that matches the filter\n\n";
+    text += "Usage: <program> --token_filter=<token_substring>\n\n";
+    text += "Options:\n";
+    text += "  -h, --help              Print this help message\n";
+    text += "  -f, --filter=<string>   Token filter substring.\n";
+    text +=
+      "                          This string is checked for substring match against token symbols\n";
+    text += "                          Eg: --filter=DFI\n";
+    return text;
+  };
+  if (!args.filter || args.help) {
+    console.log(getHelpText());
+    Deno.exit(1);
+  }
+  return { filter: args.filter };
+}
 
 async function run(cli: CliDriver, filter: string) {
   const height = await cli.getBlockHeight();
   const hash = await cli.getBlockHash(height);
   console.log(`height: ${height.value}, hash: ${hash.value}`);
   console.log("rpc: logaccountbalances");
-  console.log("note: logaccountbalances does not include pool pair rewards\n\
+  console.log(
+    "note: logaccountbalances does not include pool pair rewards\n\
     for some addresses. This is not an exact account of balances\n\
-    but a fast way in exchange for minor inaccuracies.");
+    but a fast way in exchange for minor inaccuracies.",
+  );
 
   const x = await cli.logAccountBalances();
 
   const tokens = await cli.listTokens({ limit: 1000 });
-  const tokenIds: { id: string; symbol: string; details: ListTokensItem | GetPoolPairResponse }[] = [];
+  const tokenIds: {
+    id: string;
+    symbol: string;
+    details: ListTokensItem | GetPoolPairResponse;
+  }[] = [];
   for (const id in tokens) {
     const x = tokens[id];
     if (x.symbol.includes(filter) && (x.isDAT || x.isLPS || x.isLoanToken)) {
@@ -28,12 +70,20 @@ async function run(cli: CliDriver, filter: string) {
 
   for (const addr in x.accounts) {
     const t = x.accounts[addr];
-    const res = t.map(x => new TokenAmount(x))
-      .filter(x => {
-        const match = tokenIds.find(v => v.id === x.token());
+    const res = t.map((x) => new TokenAmount(x))
+      .filter((x) => {
+        const match = tokenIds.find((v) => v.id === x.token());
         return match != undefined;
       })
-      .map(x => { return { addr, val: TokenAmount.from(x.amount(), tokenIds.find(v => v.id == x.token())!.symbol) } });
+      .map((x) => {
+        return {
+          addr,
+          val: TokenAmount.from(
+            x.amount(),
+            tokenIds.find((v) => v.id == x.token())!.symbol,
+          ),
+        };
+      });
 
     results.push(...res);
   }
@@ -52,42 +102,25 @@ async function run(cli: CliDriver, filter: string) {
   }
 
   for (const [k, v] of resultGroups) {
-    const m = v.map((x) => { return { addr: x.addr, val: x.val } });
+    const m = v.map((x) => {
+      return { addr: x.addr, val: x.val };
+    });
     m.sort((a, b) => b.val.amount() - a.val.amount());
     const total = m.reduce((acc, x) => acc + x.val.amount(), 0);
-    const res = { token: k, count: m.length, total, items: m.map(x => { return { addr: x.addr, val: x.val.toString() } }) };
+    const res = {
+      token: k,
+      count: m.length,
+      total,
+      items: m.map((x) => {
+        return { addr: x.addr, val: x.val.toString() };
+      }),
+    };
     console.log(JSON.stringify(res, null, 2));
   }
 }
 
-function handleArgs() {
-  const alias = {
-    "help": "h",
-    "filter": "f",
-  };
-  const args = parseArgs(Deno.args, {
-    alias,
-    string: ["filter"],
-    boolean: ["help"],
-    "--": false,
-  });
-  const printHelpText = () => {
-    let text = "Usage: <program> --token_filter=<token_substring>\n\n";
-    text += "  -h, --help              Print this help message\n";
-    text += "  -f, --filter=<string>   Token filter substring.\n";
-    text += "                          This string is checked for substring match against token symbols\n";
-    text += "                          Eg: --filter=DFI\n";
-    console.log(text);
-  }
-  if (!args.filter || args.help) {
-    printHelpText();
-    Deno.exit(1);
-  }
-  return { filter: args.filter };
-}
-
 async function main() {
-  const { filter } = handleArgs();
+  const { filter } = loadOpts();
   initTimedLogger();
   const cli = new CliDriver();
   console.log(`cli: ${cli.path} ${cli.args.join(" ")}, filter: ${filter}`);
